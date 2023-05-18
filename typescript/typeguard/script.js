9bb3f776f863d45ece21caa4db0898b50e4189f4
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-05-17 23:45:47 -0300

    typeguard resolution

--- typescript/typeguard/script.js
+++ typescript/typeguard/script.js
@@ -1,22 +1,37 @@
 "use strict";
-async function fetchCursos() {
-    const response = await fetch("https://api.origamid.dev/json/cursos.json");
-    const data = await response.json();
-    mostrarCursos(data);
-    console.log(data);
+// 1 - Faça um fetch da API: https://api.origamid.dev/json/cursos.json
+// 2 - Defina a interface da API
+// 3 - Crie um Type Guard, que garanta que a API possui nome, horas e tags
+// 4 - Use Type Guards para garantir a Type Safety do código
+// 5 - Preencha os dados da API na tela.
+const fetchData = async () => {
+    const req = await fetch(`https://api.origamid.dev/json/cursos.json`);
+    const res = await req.json();
+    handleCursos(res);
+};
+fetchData();
+function isCurso(data) {
+    if (data &&
+        typeof data === "object" &&
+        "nome" in data &&
+        "horas" in data &&
+        "tags" in data) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
-fetchCursos();
-function mostrarCursos(data) {
-    data.map((item) => {
-        document.body.innerHTML += `
-    <div>
-      <h2 style="color: ${item.nivel === "iniciante" ? "blue" : "red"}">${item.nome}</h2>
-      <p>Horas: ${item.horas}</p>
-      <p>Aulas: ${item.aulas}</p>
-      <p>Tipo: ${item.gratuito ? "Gratuito" : "Pago"}</p>
-      <p>Tags: ${item.tags.join(", ")}</p>
-      <p>Aulas: ${item.idAulas.join(" | ")}</p>
-    </div>
-  `;
-    });
+function handleCursos(data) {
+    if (data instanceof Array) {
+        data.filter(isCurso).map((item) => {
+            document.body.innerHTML += `
+          <div>
+            <h2>${item.nome}</h2>
+            <p>${item.horas}</p>
+            <p>${item.tags.join(", ")}</p>
+          </div>
+        `;
+        });
+    }
 }
--- typescript/typeguard/script.ts
+++ typescript/typeguard/script.ts
@@ -1,4 +1,19 @@
-interface Cursos {
+// 1 - Faça um fetch da API: https://api.origamid.dev/json/cursos.json
+// 2 - Defina a interface da API
+// 3 - Crie um Type Guard, que garanta que a API possui nome, horas e tags
+// 4 - Use Type Guards para garantir a Type Safety do código
+// 5 - Preencha os dados da API na tela.
+
+const fetchData = async () => {
+    const req = await fetch(`https://api.origamid.dev/json/cursos.json`);
+    const res = await req.json();
+
+    handleCursos(res);
+};
+
+fetchData();
+
+interface dataAPI {
     nome: string;
     horas: number;
     aulas: number;
@@ -8,28 +23,30 @@ interface Cursos {
     nivel: "iniciante" | "avancado";
 }
 
-async function fetchCursos() {
-    const response = await fetch("https://api.origamid.dev/json/cursos.json");
-    const data = await response.json();
-    mostrarCursos(data);
-    console.log(data);
+function isCurso(data: unknown): data is dataAPI {
+    if (
+        data &&
+        typeof data === "object" &&
+        "nome" in data &&
+        "horas" in data &&
+        "tags" in data
+    ) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-fetchCursos();
-
-function mostrarCursos(data: Cursos[]) {
-    data.map((item) => {
-        document.body.innerHTML += `
-    <div>
-      <h2 style="color: ${item.nivel === "iniciante" ? "blue" : "red"}">${
-            item.nome
-        }</h2>
-      <p>Horas: ${item.horas}</p>
-      <p>Aulas: ${item.aulas}</p>
-      <p>Tipo: ${item.gratuito ? "Gratuito" : "Pago"}</p>
-      <p>Tags: ${item.tags.join(", ")}</p>
-      <p>Aulas: ${item.idAulas.join(" | ")}</p>
-    </div>
-  `;
-    });
+function handleCursos(data: unknown) {
+    if (data instanceof Array) {
+        data.filter(isCurso).map((item) => {
+            document.body.innerHTML += `
+          <div>
+            <h2>${item.nome}</h2>
+            <p>${item.horas}</p>
+            <p>${item.tags.join(", ")}</p>
+          </div>
+        `;
+        });
+    }
 }
